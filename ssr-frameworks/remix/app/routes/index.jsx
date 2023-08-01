5e07d05d5fdb38ad63973ae9b51ac3d1fc77e0af
Ben Elan <no-reply@benelan.dev>
2023-07-31 18:02:29 -0700

    chore(remix): update to latest calcite-components version

--- ssr-frameworks/remix/app/routes/index.jsx
+++ ssr-frameworks/remix/app/routes/index.jsx
@@ -1,44 +1,61 @@
-import { useEffect, useState } from "react";
 import {
   CalciteButton,
+  CalciteDatePicker,
   CalciteIcon,
   CalciteSlider,
 } from "@esri/calcite-components-react";
 import styles from "@esri/calcite-components/dist/calcite/calcite.css";
+import { useEffect, useState } from "react";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
 
 function Index() {
-  const [sliderValue, setSliderValue] = useState(0);
-  useEffect(() => {
-    import("@esri/calcite-components/dist/components").then(
-      ({ setAssetPath }) =>
-        setAssetPath(
-          "https://unpkg.com/@esri/calcite-components/dist/calcite/assets"
-        )
-    );
+  const [sliderValue, setSliderValue] = useState(50);
 
-    import("@esri/calcite-components/dist/components/calcite-button.js");
-    import("@esri/calcite-components/dist/components/calcite-icon.js");
-    import("@esri/calcite-components/dist/components/calcite-slider.js");
+  useEffect(() => {
+    async function registerComponents() {
+      const { setAssetPath } = await import(
+        "@esri/calcite-components/dist/components"
+      );
+      setAssetPath(
+        "https://unpkg.com/@esri/calcite-components/dist/calcite/assets"
+      );
+
+      await import("@esri/calcite-components/dist/components/calcite-button");
+      await import("@esri/calcite-components/dist/components/calcite-icon");
+      await import("@esri/calcite-components/dist/components/calcite-slider");
+      await import(
+        "@esri/calcite-components/dist/components/calcite-date-picker"
+      );
+    }
+    registerComponents();
   }, []);
 
   return (
     <>
       <h1>
-        Calcite Components in Remix.run <CalciteIcon icon="banana" scale="l" />
+        Calcite Components in Remix.run <CalciteIcon icon="banana" />
       </h1>
+
+      <CalciteDatePicker />
+
+      <p>
+        This sample demonstrates using Calcite Components (web components) in
+        Remix (SSR). The components need to be imported dynamically in useEffect
+        because they rely on browser APIs.
+      </p>
+
       <CalciteSlider
         min={1}
         max={100}
         value={sliderValue}
         step={1}
-        onCalciteSliderUpdate={(e) => setSliderValue(e.target.value)}
+        onCalciteSliderInput={(e) => setSliderValue(e.target.value)}
       />
-      <p>The slider currently has a value of {sliderValue}.</p>
-      <CalciteButton onClick={() => setSliderValue(100)}>Set Max Value</CalciteButton>
+      <p>The slider currently has a value of {sliderValue}</p>
+      <CalciteButton onClick={() => setSliderValue(0)}>Clear</CalciteButton>
     </>
   );
 }
