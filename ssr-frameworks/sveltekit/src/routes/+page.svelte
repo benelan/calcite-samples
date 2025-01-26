f3e0ad150e885f246390e37e6ff9aefa77913836
Ben Elan <no-reply@benelan.dev>
2025-01-26 15:14:56 -0800

    feat: add sveltekit/jsapi example

---
+++ ssr-frameworks/sveltekit/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import MapView from "@arcgis/core/views/MapView"
+    import "@arcgis/core/assets/esri/themes/light/main.css"
+
+    const createMap = (domNode) => {
+        view = new MapView({
+            container: domNode,
+            map: {
+                basemap: "topo-vector",
+            },
+            center: [5.25, 52.14],
+            zoom: 8,
+        })
+    }
+</script>
+
+<main>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <div use:createMap></div>
+</main>
